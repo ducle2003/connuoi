58e654dbd0777bdb49d89e9dec464ce1158d3da1
ducle2003 <duclee003@gmail.com>
2022-03-30 20:24:25 +0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "¡Hola, mundo!"   
+    end
+
+    def welcome
+        render html: "welcome to my project!"
+    end
+end  
