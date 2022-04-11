864e76fbd7381c1deb6823f3bfad94c8ebac1a74
ducle2003 <duclee003@gmail.com>
2022-04-11 22:36:20 +0700

    chap_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 end
